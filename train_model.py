b83a382a13bd2be1e43f0cffa3ad96f05ee53b2b
Steven Ngo <svinhngo@gmail.com>
2025-07-15 21:43:01 -0700

    Update train_model.py: argument parser and imports

--- train_model.py
+++ train_model.py
@@ -3,16 +3,19 @@ import matplotlib
 matplotlib.use("Agg")
 
 # import the necessary packages
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
-from tensorflow.keras.callbacks import LearningRateScheduler
-from tensorflow.keras.optimizers import SGD
-from pyimagesearch.resnet import ResNet
-from pyimagesearch import config
-from sklearn.metrics import classification_report
-from imutils import paths
-import matplotlib.pyplot as plt
-import numpy as np
-import argparse
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import ImageDataGenerator # for data augmentation
+from tensorflow.keras.callbacks import LearningRateScheduler # for learning rate scheduler
+from tensorflow.keras.optimizers import SGD # stochastic gradient descent
+from tensorflow.keras.utils import to_categorical # for one-hot encoding
+from main.resnet import ResNet # for the ResNet model
+from main import config # for the configuration
+from sklearn.metrics import classification_report # for the classification report
+from imutils import paths # for the paths
+import matplotlib.pyplot as plt # for the plot
+import numpy as np # for the numpy
+import argparse # for the argument parser
+
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-p", "--plot", type=str, default="plot.png",
